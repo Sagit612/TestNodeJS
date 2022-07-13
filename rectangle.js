ffbce87b0bef56061e84cb55db06443b4ef933f2
sagit <minhquoc1423@gmail.com>
2022-07-14 00:19:40 +0700

    add explanation

--- rectangle.js
+++ rectangle.js
@@ -1,17 +1,23 @@
 module.exports = (x,y,callback) =>{
     if (x <= 0 || y <= 0)
-    setTimeout(()=>
-        callback(new Error("Rectangle dimensions should be greater than zero: l = "
-            + x + ", and b = " + y),
-            null),
-            2000
-    )
+    // call setTimeout() function
+        setTimeout(()=> 
+            // callback() function here is a first parameter of the outer function 
+            // callback() function has two functions passed inside 
+            callback(new Error(("Rectangle dimensions should be greater than zero: l = " + x + ", and b = " + y),
+                null), 
+            2000)
+        )
     else
         setTimeout(() =>
+            // callback() function here is a first parameter of the outer function 
+            // callback() function has two functions passed inside 
             callback(null, {
-                perimeter: () => (2*(x+y)),
-                area:() => (x*y)
-            }),
-            2000
-    )
+                    perimeter: (x, y) => (2*(x+y)),
+                    area: (x, y) => (x*y)
+                }
+                // this {} is the object having two (key: value)s, 
+                // key here is the same as the function, and value is the logic of the key
+                ),
+            2000)
 }
